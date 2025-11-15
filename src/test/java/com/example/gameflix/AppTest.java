e4161ae622213ffe93a114c54cb6e8a24f99f314
mcj02 <mcj5290@psu.edu>
2025-11-14 22:38:32 -0500

    Update AppTest.java

--- src/test/java/com/example/gameflix/AppTest.java
+++ src/test/java/com/example/gameflix/AppTest.java
@@ -1,5 +1,9 @@
 package com.example.gameflix;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class AppTest {
 
