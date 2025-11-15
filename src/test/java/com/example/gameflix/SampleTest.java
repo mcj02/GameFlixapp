e7711fe17eda0363279c8b3b8fac8c9526ba647a
mcj02 <mcj5290@psu.edu>
2025-11-14 22:26:13 -0500

    Create SampleTest.java

---
+++ src/test/java/com/example/gameflix/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example.gameflix
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SampleTest {
+
+    @Test
+    void additionTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
